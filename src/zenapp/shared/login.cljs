53fb510066923a6152a27fffdcacb903472016da
rthadani <rohit.thadani@gmail.com>
2017-03-09 17:54:11 -0800

    sucessfully loggedinto fb

--- src/zenapp/shared/login.cljs
+++ src/zenapp/shared/login.cljs
@@ -1,20 +1,30 @@
 (ns zenapp.shared.login
   (:require [zenapp.shared.reactcomponents :refer [view alert fb-login-button fb-access-token]]))
 
+(defn get-access-token
+  [success-response]
+  (let [access-token (.. success-response -accessToken)]
+    (js/console.log "callback?")
+    (js/console.log (.toString access-token))
+    (.toString access-token)))
+
+(defn login-finished
+  [error result]
+  (cond
+    error  (js/console.log "this is error") #_(.alert alert (str "login has error " (.-error result)))
+    #_(.isCancelled result) #_(js/console.log "loginn is cancelled")
+    :else (-> (.getCurrentAccessToken fb-access-token)
+              (.then get-access-token #(js/console.log %)))))
+
 (defn login-view
   []
   (fn []
     [view ]
     [fb-login-button  {:publish-permissions ["publish_actions"]
                        :on-login-finished login-finished
-                       :on-logout-finished logout-finished
-                       } ]) )
+                       :on-logout-finished logout-finished}]) )
 
 
-(defn login-finished [error result]
-  (cond
-    error (.alert alert (str "login has error " (.error result)))
-    (.isCancelled result) (.alert alert "loginn is cancelled")
-    :else (-> fb-access-token  .getCurrentAccessToken (.then #(.alert alert %)))) )
+
 
 
--- src/zenapp/shared/reactcomponents.cljs
+++ src/zenapp/shared/reactcomponents.cljs
@@ -15,5 +15,10 @@
 (def mapview-marker (r/adapt-react-class (.-Marker MapView)))
 (def scrollview (r/adapt-react-class (.-ScrollView ReactNative)))
 (def fb-login-button (r/adapt-react-class (.-LoginButton FBSDK)))
-(def fb-access-token (r/adapt-react-class (.-AccessToken FBSDK)))
+#_(def fb-access-token (r/adapt-react-class FBSDK))
+
+(set! js/window.FBSDK FBSDK)
+(def fbsdk-static js/window.FBSDK)
+(def fb-access-token (.-AccessToken fbsdk-static))
+
 
